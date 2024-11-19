e14ecc3340340de931aa959bd14076432dfcf107
AyScorp <aymericcherbonnier@gmail.com>
2024-11-19 09:51:57 +0100

    Ajout de la page produit avec la structure HTML, le style CSS et la fonctionnalité JavaScript pour la galerie d'images.

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function redirectToProduct() {
     window.location.href = 'produit.html';
   }
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const thumbnails = document.querySelectorAll('.first-img img');
+    const mainImage = document.getElementById('main-image');
 
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            mainImage.src = this.src;
+        });
+    });
+});
 
