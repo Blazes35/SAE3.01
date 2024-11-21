05e14abd1db1fbd61dc1c09ba1f2862364a292dd
AyScorp <aymericcherbonnier@gmail.com>
2024-11-21 16:24:43 +0100

    Réorganisation des fichiers et mise à jour des chemins d'accès ; ajout d'un nouveau répertoire pour les images et amélioration des styles de la galerie.

--- calendrier.php
+++ calendrier.php
@@ -27,7 +27,7 @@
                     <a href="GestionProfilAdmin.php" class="profils" style="cursor: pointer;">GESTION PROFILS</a>
                     <a href="tresorie.php" class="tresorie" style="cursor: pointer;">TRÉSORIE</a>
                     <a href="parametres.html" class="parametres" style="cursor: pointer;">PARAMÈTRES</a>
-                    <a href="/php/boutique_hugo.php" class="editer" style="cursor: pointer;">EDITER CONTENU</a>
+                    <a href="boutique_hugo.php" class="editer" style="cursor: pointer;">EDITER CONTENU</a>
                 </div>
         </div>
 </div>
