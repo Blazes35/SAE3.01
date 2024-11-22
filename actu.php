0f5af253963cf218a9bc531a89b06725eddfd3b0
AyScorp <aymericcherbonnier@gmail.com>
2024-11-22 09:57:42 +0100

    Mise à jour de la page d'actualité pour la renommer en galerie et ajout d'un en-tête avec des liens de navigation; création d'un nouveau fichier CSS pour le style trie des fichiers img dans différents dossier

--- actu.php
+++ actu.php
@@ -3,9 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualité</title>
+    <title>Galerie</title>
+    <!-- Lien pour importer les Material Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link rel="stylesheet" href="actu.css" />
+    <link rel="stylesheet" href="header.css" />
 </head>
 <body>
+    <header>
+        <div class="overlap-group">
+            <img class="logo" src="../images/logo.png" />
+            <div class="theme-claire">THEME CLAIRE</div>
+        </div>
+        <div class="overlap-group-2">
+            <span class="material-symbols-outlined">account_circle</span>
+            <div class="mon-compte">MON COMPTE</div>
+            <span class="material-symbols-outlined">shopping_cart</span>
+        </div>
+    </header>
+    <div class="box">
+        <div class="rectangle">
+            <div class="titre-de-page">
+                <div class="overlap-group-3">
+                    <a href="presentation.html" class="presentation" style="cursor: pointer;">QUI SOMMES NOUS</a>
+                    <a href="evenement.html" class="evenement" style="cursor: pointer;">ÉVENEMENTS</a>
+                    <a href="calendrier.html" class="calendrier" style="cursor: pointer;">CALENDRIER</a>
+                    <a href="galerie.html" class="galerie" style="cursor: pointer;">GALERIE</a>
+                    <a href="boutique.html" class="boutique" style="cursor: pointer;">BOUTIQUE</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php
     $connect = new PDO('mysql:host=localhost;dbname=inf2pj_02', 'root', '');
     $queryActu = "SELECT titreActualite, descActualite, dateActualite, urlPhotoActualite FROM ACTUALITE"; 
