17af2b9548a952da6d3f713471b55ddd8e3b4174
mathilde2801 <mathilde72m@gmail.com>
2024-11-24 03:21:44 +0100

    Commencement panier

--- panier.php
+++ panier.php
@@ -5,6 +5,8 @@ session_start();
 
 $_SESSION['email'] = 'leo.lopez@gmail.com';
 
+$total = 0;
+
 $connection = new PDO('mysql:host=localhost;dbname=inf2pj_02', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -15,8 +17,8 @@ CODEPROMO.idCode, nomCode, dateFin, pourcentCode, condtitionCode
 FROM COMMANDE JOIN utilisateur
 ON COMMANDE.idUser = utilisateur.idUser
 JOIN PRODUIT ON COMMANDE.idProd = PRODUIT.idPROD
-JOIN APPLIQUER ON PRODUIT.idProd = APPLIQUER.idProd
-JOIN CODEPROMO ON CODEPROMO.idCode = APPLIQUER.idCode
+LEFT JOIN APPLIQUER ON PRODUIT.idProd = APPLIQUER.idProd
+LEFT JOIN CODEPROMO ON CODEPROMO.idCode = APPLIQUER.idCode
  WHERE adrMailUser= :email";
 
 $stmt = $connection->prepare($sql);
@@ -26,16 +28,46 @@ $stmt->bindParam(':email', $_SESSION['email'], PDO::PARAM_STR);
 $stmt->execute();
 
 $commandes = $stmt->fetchAll();
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panier</title>
+    <link rel="stylesheet" href="panier.css" />
+</head>
+<body>
+<?php
 if($commandes){
     echo '<div class="commandes">';
+    echo '<div class="commande">';
+        echo '<div class="image">
+            Images
+        </div>';
+        echo '<div class="numCommande">
+            Numéro de la commande
+        </div>';
+        echo '<div class="nomProduit">
+            Nom du produit
+        </div>';
+        echo '<div class="quantiteProduit">
+            Quantité
+        </div>';
+        echo '<div class="prixProduit">
+            Prix Unitaire
+        </div>';
+
+    echo'</div>';
     foreach($commandes as $commande){
         echo '<div class="commande">';
+            echo'<div class="image">';
             if(!empty($commande['imgProd'])){
-                echo'<img src="' . $commande['imgProd'] . '" alt="' . $commande['nomProd'] . '" />';
+                echo '<img src="uploads/produits/' . $commande['imgProd'] . '" alt="' . $commande['nomProd'] . '" />';
             }else{
                 echo '<img src="/images/avatar.png" alt="default image"/>';
             }
+            echo '</div>';
             echo '<div class="numCommande">';
                 echo '<p>'. $commande['idCommande'].'</p>';
             echo '</div>';
@@ -48,8 +80,14 @@ if($commandes){
             echo '<div class="prixProduit">';
                 echo'<p>'.$commande['prixProd'].'</p>';
             echo'</div>';
+            $total = $total + $commande['quantiteCommande'] * $commande['prixProd'];
         echo'</div>';
     }
+    echo '<div class="total">';
+        echo "Total: " . number_format($total, 2) . " €"; 
+    echo '</div>';
     echo'</div>';
 }
-?>
+?> 
+</body>
+</html>
