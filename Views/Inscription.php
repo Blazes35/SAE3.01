ce97ef9c363c473f7e10a930c497ef55daa3c6f7
AyScorp <aymericcherbonnier@gmail.com>
2024-11-28 14:17:52 +0100

    Suppression du script de récupération des données de session dans la vue d'inscription et mise à jour de l'index pour inclure le contrôleur d'inscription.

--- Views/Inscription.php
+++ Views/Inscription.php
@@ -7,15 +7,6 @@
         <p><?php echo $message; ?></p>
     </div>
 
-<script>
-    // Récupération des données de session envoyées depuis PHP
-    var userRole = <?php echo json_encode($userRole); ?>;
-    var userName = <?php echo json_encode($userName); ?>;
-
-    // Affichage des informations dans la console
-    console.log("Role de l'utilisateur : " + userRole);
-    console.log("Nom de l'utilisateur : " + userName);
-</script>
 
 
 </body>
--- index.php
+++ index.php
@@ -55,6 +55,8 @@ if (!$_SESSION['adminPanel']){
         case 'DetailEvent' :
             include "./Controllers/DetailEventController.php";
             break;
+        case 'Inscription' :
+            include "./Controllers/InscriptionController.php" ;
         default:
             include './Views/Error404.php';
             break;
@@ -76,9 +78,6 @@ if (!$_SESSION['adminPanel']){
             break;
         case 'Profile' :
             include "./GestionProfilAdmin.php";
-            break;
-        case 'Inscription' :
-            $role < 4 ? include "./Controllers/InscriptionController.php" : include './Views/Error404.php';
             break;
         case 'GestionProfilAdmin' :
             include "./Controllers/GestionProfilAdminController.php";
