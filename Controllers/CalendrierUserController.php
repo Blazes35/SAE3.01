7ba425880a0d9cb7aadc8a8a28621c9fcd0e07db
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2024-11-29 09:59:53 +0100

    Correction bug calendrier

--- Controllers/CalendrierUserController.php
+++ Controllers/CalendrierUserController.php
@@ -5,7 +5,7 @@ require_once 'Models/CalendrierUserModel.php';
 
 $userRole = isset($_SESSION['role']) ? $_SESSION['role'] : 0;
 $userName = isset($_SESSION['nom']) ? $_SESSION['nom'] : 'Invité';
-$TP = isset($_POST['TP']) ? $_POST['TP'] : null;
+$TP = isset($_SESSION['TP']) ? $_SESSION['TP'] : null;
 
 if (!$TP) {
     die("Erreur : ID TP Agenda manquant ou invalide.");
